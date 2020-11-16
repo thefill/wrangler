55c3c0958c166c0ab8b52bd1ae84d39f3e0dbb4d
Nat <davidson@cloudflare.com>
2020-11-16 14:41:06 -0500

    First pass at integration test for publish

---
+++ tests/curlkv.sh
@@ -0,0 +1,7 @@
+curl --location --request GET 'https://api.cloudflare.com/client/v4/accounts/04e8ec7979d1d07c39925ab7332a116e/storage/kv/namespaces/4145d302cffa4084b8d27938d85abd68/keys' \
+--header 'X-Auth-Email: davidsonnatalies@gmail.com' \
+--header 'X-Auth-Key: 38682d41f34304a6b65a747d763f738ab3ceb' \
+--header 'Content-Type: application/json' \
+--header 'Authorization: Bearer iLP8xj3bqi_3FScRpixMwIKHBP7MgUJkiIn6Q5R8' \
+--header 'Cookie: __cfduid=d3edcbc2c2776e49ac2ff3b5addbfd1441604678420; __cflb=0H28vgHxwvgAQtjUGU4vq74ZFe3sNVUZeLpq8Xntnp5' \
+--data-raw ''
